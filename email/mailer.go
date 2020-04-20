7e16a9a774636665ac22da605f417e2d74cc419f
Scott Finlay <scott.p.finlay@gmail.com>
2020-04-20 17:53:17 +0200

    Forgot to git add

---
+++ email/mailer.go
@@ -0,0 +1,41 @@
+package email
+
+import (
+	"github.com/mailjet/mailjet-apiv3-go"
+	"github.com/ynori7/music/config"
+)
+
+type Mailer struct {
+	config config.Config
+	emailClient *mailjet.Client
+}
+
+func NewMailer(conf config.Config) Mailer {
+	return Mailer{
+		config: conf,
+		emailClient: mailjet.NewMailjetClient(conf.Email.PublicKey, conf.Email.PrivateKey),
+	}
+}
+
+func (m Mailer) SendMail(subject string, htmlBody string) error {
+	messagesInfo := []mailjet.InfoMessagesV31 {
+		{
+			From: &mailjet.RecipientV31{
+				Email: m.config.Email.From.Address,
+				Name: m.config.Email.From.Name,
+			},
+			To: &mailjet.RecipientsV31{
+				mailjet.RecipientV31 {
+					Email: m.config.Email.To.Address,
+					Name: m.config.Email.To.Name,
+				},
+			},
+			Subject: subject,
+			HTMLPart: htmlBody,
+		},
+	}
+	messages := mailjet.MessagesV31{Info: messagesInfo }
+	_, err := m.emailClient.SendMailV31(&messages)
+
+	return err
+}
